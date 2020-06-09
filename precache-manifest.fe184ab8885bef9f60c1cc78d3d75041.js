bcd458b15cea5588b0f18b3d9e8de55cba4855b5
Arnold Amasoh <amasoharnold@gmail.com>
2020-06-08 15:32:55 -1100

    Updates

---
+++ precache-manifest.fe184ab8885bef9f60c1cc78d3d75041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a1eec75ee8ba09936eecf07bee507f",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "3d54702a005670865e8c",
+    "url": "/face-detection/static/css/2.bff19397.chunk.css"
+  },
+  {
+    "revision": "332cd1976a306cb7f447",
+    "url": "/face-detection/static/css/main.eeb383f4.chunk.css"
+  },
+  {
+    "revision": "3d54702a005670865e8c",
+    "url": "/face-detection/static/js/2.e91b6c6e.chunk.js"
+  },
+  {
+    "revision": "332cd1976a306cb7f447",
+    "url": "/face-detection/static/js/main.76f9bcd9.chunk.js"
+  },
+  {
+    "revision": "31d5d7e2a87a451484b6",
+    "url": "/face-detection/static/js/runtime~main.2fd50e09.js"
+  }
+]);
